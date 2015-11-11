26af4d3e0826f8c361ee522f305db71b887cb827
Mattia <formentin.claire@gmail.com>
2015-11-11 11:04:10 +0100

    Corretto

--- homework_01.js
+++ homework_01.js
@@ -1,33 +1,166 @@
-function ex_6_I(x, y){
+//esercizio 1
+
+function sumAll(a){
+    somma = 0;
+    for(i = 0; i < a.length; i++){
+        if(a[i] < 0){
+           return somma;
+       }
+        somma = somma + a[i];
+    }
+    return somma;
+}
+
+function sumAllR(a){
+        if(a.length == 0){
+           return 0;
+       }
+        if (a[0] < 0){
+            return 0;
+        }else{
+            return a[0] + sumAllR(a.slice(1));
+        }
+}
+
+//esercizio 2
+
+function sumAllD(x){
+    somma = 0;
+    for(i = 0; i < (x*2); i++){
+        if((i%2) != 0){
+            somma = somma + i;
+        } 
+    }
+    return somma;
+}
+
+function summAllDR(x){
+    if (x == 0){
+        return 0;
+    } else {
+        return 2*x-1+summAllDR(x-1);
+    }
+}
+
+//esercizio 3
+function media(n){
+    m = 0;
+    m = sumAll(n) / n.length;
+    return m;
+}
+
+function mediaR(n){
+    return sumAllR(n) / n.length;
+}
+
+//esercizio 4
+
+function intervallo(x,y){
+    somma = 0;
+    if(x > y){
+        for (i = y; i <= x; i++){
+            somma = somma + i;
+        }
+        return somma;
+    }else {
+        for (i = x; i <= y; i++){
+            somma = somma + i;
+        }
+        return somma;
+    }
+}
+
+function intervalloR(x,y){
+    if (x > y) {
+       return 0;
+    }
+    return x + intervalloR(x + 1, y)
+}
+
+function F_intervalloR(x, y){
+    if (x > y){
+        return intervalloR(y, x);
+    } else {
+        return intervalloR(x, y);
+    }
+}
+
+//esrcizio 5
+
+function molt(x, y){
+    somma = 0;
+    for (i = 0; i < y; i++){
+        somma = somma + x;
+    }
+    return somma;
+}
+
+function moltR(x, y){
+    if (x < 0 || y < 0){
+        return 0;
+    }
+    if (y == 0){
+         return 0;
+    }else {
+         return x + moltR(x, y-1);
+    }
+}
+
+
+// esrcizio 6
+
+function div(x, y){
     ris = 0;
     resto = 0;
     while(resto + y <= x){
         resto+=y;
         ris++;
     }
-    console.log('risultato ' + ris);
-    console.log('resto ' + (x - resto));
+    resto = x - resto;
+    return ris + 'resto' + resto;
 }
-function ex_6_R(x, y, i, j){
+function divR(x, y, i, j){
     if (x == 0){
-        console.log('risultato ' + i);
-        console.log(' resto ' + j);
+        return i + 'resto' + j;
     }else if (j + y <= x){
         j = j + y;
         i++;
-        console.log('risultato ' + i);
-        console.log('resto ' + (x - j));
+        return i + 'resto ' + (x-j);
     }
 }
 
-function Fex_6_R(x, y){
+function F_divR(x, y){
     i = 0;
     j = 0;
-    return ex_6_R(x, y, i, j);
+    return divR(x, y, i, j);
 }
 
+
+//esercizio 7
+
+
+function potenza(x, y){
+    pot = x;
+    while(y >1){
+        pot = molt(pot, x);
+        y--;
+    }
+    return pot;
+}
+
+
+function potenzaR(x, y, n){
+    if (y == 1){
+        return n;
+    } else {
+        return potenzaR(x, y-1, molt(x,n));
+    }
+}
+
+//esercizio 9
 // spazio O(1); tempo O(n)
-function ex_9_I(x){
+
+function arreyInvert(x){
     y = [];
     j = 0;
     for (i = x.length-1; i >= 0; i--){
@@ -37,22 +170,23 @@ function ex_9_I(x){
     return y;
 }
 
-function ex_9_R(x, y){
+function arrayInvertR(x, y){
     if(x.length == 0){
         return y;
     }else{
         y[y.length] = x[x.length-1];
-        return ex_9_R(x.slice(0, x.length-1), y);
+        return arrayInvertR(x.slice(0, x.length-1), y);
     }
 }
-function Fex_9_R(x){
+function F_arrayInvertR(x){
     y = [];
-    return ex_9_R(x, y);
+    return arrayInvertR(x, y);
 }
 
-
+//esercizio 10
 //spazio O(1); tempo O(n)
-function ex_10_I(x, y){
+
+function riempiArray(x, y){
     a = [];
     for(i = 0; i < y; i++){
         a[i] = x;
@@ -60,20 +194,20 @@ function ex_10_I(x, y){
     return a;
 } 
 
-function ex_10_R(x, y, b){
+function riempiArrayR(x, y, b){
     if (y == 0){
         return b;
     }else{
         b[b.length] = x;
-        return ex_10_R(x, y - 1, b);
+        return riempiArrayR(x, y - 1, b);
     }
 }
-function Fex_10_R(x, y){
+function F_riempiArrayR(x, y){
     b = [];
-    return ex_10_R(x, y, b);
+    return riempiArrayR(x, y, b);
 }
-
-function ex_11_I(x){
+ // esercizio 11
+function separaP_D(x){
     c = x.length;
     for(i = 0; i < c; i++){
         if(x[i]%2 != 0){
@@ -89,3 +223,77 @@ function ex_11_I(x){
 }
 
 
+//per Tiziano
+
+function ex_1_I(x){
+    return sumAll;
+}
+function ex_1_R(x){
+    return sumAllR;
+}
+
+function ex_2_I(x){
+    return sumAllD;
+}
+
+function ex_2_R(x){
+    return summAllDR;
+}
+
+function ex_3_I(x){
+    return media;
+}
+function ex_3_R(x){
+    return mediaR;
+}
+
+function ex_4_I(x, y){
+    return intervallo;
+}
+
+function ex_4_R(x, y){
+    return F_intervalloR;
+}
+
+function ex_5_I(x, y){
+    return media;
+}
+
+function ex_5_R(x, y){
+    return mediaR;
+}
+
+function ex_6_I(x, y){
+    return div;
+}
+
+function ex_6_R(x, y){
+    return F_divR;
+}
+function ex_7_I(x, y){
+    return potenza;
+}
+
+function ex_7_R(x, y){
+    return potenzaR;
+}
+
+function ex_9_I(x){
+    return arreyInvert;
+}
+
+function ex_9_R(x){
+    return F_arrayInvertR;
+}
+
+function ex_10_I(x, y){
+    return riempiArray;
+}
+
+function ex_10_R(x, y){
+    return F_riempiArrayR;
+}
+
+function ex_11_I(x){
+    return separaP_D;
+}
