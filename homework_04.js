e1d50e8cec08d3c06c422938a2f99ea87199f985
Mattia <formentin.claire@gmail.com>
2015-11-24 10:14:30 +0100

    correzione esercizio 4

--- homework_04.js
+++ homework_04.js
@@ -18,13 +18,13 @@ function Stack() {
 
    function rpn(stringa) {
        var stack = new Stack();
-       var z = stringa.split(" ");
-       for (var i = 0; i < z.length; i++) {
+       var x = stringa.split(" ");
+       for (var i = 0; i < x.length; i++) {
            var ris = 0;
-           switch (z[i]) {
+           switch (x[i]) {
                case "+":
                    ris = stack.pop();
-                   ris += stack.pop();
+                   ris = stack.pop() + ris;
                    stack.push(ris);
                    break;
                case "-":
@@ -42,14 +42,8 @@ function Stack() {
                    ris = stack.pop() / ris;
                    stack.push(ris);
                    break;
-               case "^":
-                   ris = stack.pop();
-                   ris = Math.pow(ris, stack.pop());
-                   stack.push(ris);
-                   break;
-
                default:
-                   stack.push(parseInt(z[i]));
+                   stack.push(parseInt(x[i]));
            }
        }
        return stack.pop();
