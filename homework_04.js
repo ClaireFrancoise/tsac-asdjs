dc6a80ed0889a751b6c44bfe0b31f75bd7a35436
Mattia <formentin.claire@gmail.com>
2015-11-24 09:52:54 +0100

    correzione esercizio 4

--- homework_04.js
+++ homework_04.js
@@ -1,33 +1,56 @@
-function rpn(stringa) {
-      var stack = new Stack();
-      var ss = stringa.split(" ");
-      for (var i = 0; i < ss.length; i++) {
-          var ris = 0;
-          switch (ss[i]) {
-              case "+":
-                  ris = stack.pop();
-                  ris += stack.pop();
-                  stack.push(ris);
-                  break;
-              case "-":
-                  ris = stack.pop();
-                  ris -= stack.pop();
-                  stack.push(ris);
-                  break;
-              case "*":
-                  ris = stack.pop();
-                  ris *= stack.pop();
-                  stack.push(ris);
-                  break;
-              case "/":
-                  ris = stack.pop();
-                  ris /= stack.pop();
-                  stack.push(ris);
-                  break;
-           
-              default:
-                  stack.push(parseInt(ss[i]));
-          }
-      }
-      return stack.pop();
-  }
+function Stack() {
+       this.array = [];
+   }
+
+   Stack.prototype.push = function (insert) {
+       this.array.push(insert);
+   }
+   Stack.prototype.pop = function () {
+       return this.array.pop();
+   }
+   Stack.prototype.peek = function () {
+       return this.array[this.array.length - 1];
+   }
+   Stack.prototype.isEmpty = function () {
+       return this.array.length == 0;
+   }
+
+
+   function rpn(stringa) {
+       var stack = new Stack();
+       var z = stringa.split(" ");
+       for (var i = 0; i < z.length; i++) {
+           var ris = 0;
+           switch (z[i]) {
+               case "+":
+                   ris = stack.pop();
+                   ris += stack.pop();
+                   stack.push(ris);
+                   break;
+               case "-":
+                   ris = stack.pop();
+                   ris = stack.pop() - ris;
+                   stack.push(ris);
+                   break;
+               case "*":
+                   ris = stack.pop();
+                   ris *= stack.pop();
+                   stack.push(ris);
+                   break;
+               case "/":
+                   ris = stack.pop();
+                   ris = stack.pop() / ris;
+                   stack.push(ris);
+                   break;
+               case "^":
+                   ris = stack.pop();
+                   ris = Math.pow(ris, stack.pop());
+                   stack.push(ris);
+                   break;
+
+               default:
+                   stack.push(parseInt(z[i]));
+           }
+       }
+       return stack.pop();
+   }
